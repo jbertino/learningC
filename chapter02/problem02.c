023d1f3c8c8a3b3d4f6c7c8490829878087bd27e
Bert_the_Dad <theterbino@gmail.com>
2025-05-26 16:17:45 -0700

    updated problem02 and completed problem03 & problem04 (#4)

---
+++ chapter02/problem02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define PI 3.1415926535f
+
+int main(void) {
+    float radius = 10.0f;
+    float volume = (4.0f/3.0f)*PI*radius*radius*radius;
+    printf("%.10f\n", volume);
+
+    return 0;
+}
